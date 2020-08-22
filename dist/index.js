ebe4da2b2f8ef8114951c00f9078299a646eea37
Marlrus <36910211+Marlrus@users.noreply.github.com>
2020-08-21 20:28:31 -0500

    completed iter, range, pyRange, randomInt, logger, head, first, last, tail, initial

--- dist/index.js
+++ dist/index.js
@@ -6,5 +6,56 @@ export const hmm = (vars) => {
         console.log(vars);
     }
 };
-hmm('hai');
+export const err = (message) => {
+    if (message === undefined) {
+        throw new Error(`¯\_(ツ)_/¯`);
+    }
+    else {
+        throw new Error(message);
+    }
+};
+export const iter = (loops) => [...Array(loops).keys()];
+export const range = (end, start = 0, step = 1) => {
+    if (end < 0)
+        err('End param cannot be a negative integer.');
+    if (start > end)
+        err('Start cannot be after end param.');
+    if (start < 0)
+        err('Start has to be a positive integer.');
+    if (step <= 0)
+        err('Step has to be a positive integer greater than 0.');
+    const length = Math.ceil((end - start) / step);
+    const arr = [...Array(length).keys()];
+    return arr.map(x => x + start + x * (step - 1));
+};
+export const pyRange = (start, end, step = 1) => {
+    if (end === undefined) {
+        end = start;
+        start = 0;
+    }
+    if (end < 0)
+        err('End param cannot be a negative integer.');
+    if (start > end)
+        err('Start cannot be after end param.');
+    if (start < 0)
+        err('Start has to be a positive integer.');
+    if (step <= 0)
+        err('Step has to be a positive integer greater than 0.');
+    const length = Math.ceil((end - start) / step);
+    const arr = [...Array(length).keys()];
+    return arr.map(x => x + start + x * (step - 1));
+};
+export const randomInt = (top, start = 0) => {
+    const range = top - start + 1;
+    return Math.floor(Math.random() * range + start);
+};
+export const logger = (x) => {
+    hmm(x);
+    return x;
+};
+export const head = (arr) => arr.slice(0, 1)[0];
+export const first = head;
+export const last = (arr) => arr.slice(arr.length - 1)[0];
+export const tail = (arr) => arr.slice(1, arr.length);
+export const initial = (arr) => arr.slice(0, arr.length - 1);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-export const hmm = (vars: any) => {
+export const hmm = (vars?: any) => {
 	if (vars === undefined || vars === null) {
 		console.log(`ಠ_ಠ`);
 	} else {
@@ -6,27 +6,29 @@ export const hmm = (vars: any) => {
 	}
 };
 
-hmm('hai');
+export const err = (message?: string) => {
+	if (message === undefined) {
+		throw new Error(`¯\_(ツ)_/¯`);
+	} else {
+		throw new Error(message);
+	}
+};
 
-// export const err = message => {
-// 	if (message === undefined) {
-// 		throw new Error(`¯\_(ツ)_/¯`);
-// 	} else {
-// 		throw new Error(message);
-// 	}
-// };
+export const iter = (loops: number) => [...Array(loops).keys()];
 
-// export const iter = loops => [...Array(loops).keys()];
+export const range = (end: number, start = 0, step = 1) => {
+	// if (end < 0 || start > end || step <= 0) return;
+	if (end < 0) err('End param cannot be a negative integer.');
+	if (start > end) err('Start cannot be after end param.');
+	if (start < 0) err('Start has to be a positive integer.');
+	if (step <= 0) err('Step has to be a positive integer greater than 0.');
 
-// export const range = (end, start = 0, step = 1) => {
-// 	if (end < 0 || start > end || step <= 0) return;
+	const length = Math.ceil((end - start) / step);
 
-// 	const length = Math.ceil((end - start) / step);
+	const arr = [...Array(length).keys()];
 
-// 	const arr = [...Array(length).keys()];
-
-// 	return arr.map(x => x + start + x * (step - 1));
-// };
+	return arr.map(x => x + start + x * (step - 1));
+};
 
 // // const emptyArr = Array(5);
 
@@ -36,51 +38,70 @@ hmm('hai');
 
 // // hmm({ finalArr });
 
-// export const pyRange = (start, end, step = 1) => {
-// 	if (end < 0 || start > end || step <= 0) return;
+export const pyRange = (start: number, end?: number, step = 1) => {
+	if (end === undefined) {
+		end = start;
+		start = 0;
+	}
 
-// 	if (end === undefined) {
-// 		end = start;
-// 		start = 0;
-// 	}
+	if (end < 0) err('End param cannot be a negative integer.');
+	if (start > end) err('Start cannot be after end param.');
+	if (start < 0) err('Start has to be a positive integer.');
+	if (step <= 0) err('Step has to be a positive integer greater than 0.');
 
-// 	const length = Math.ceil((end - start) / step);
+	const length = Math.ceil((end - start) / step);
 
-// 	const arr = [...Array(length).keys()];
+	const arr = [...Array(length).keys()];
 
-// 	return arr.map(x => x + start + x * (step - 1));
-// };
+	return arr.map(x => x + start + x * (step - 1));
+};
+
+// type MapFunction<T> = (value: T, index?: number, array?: any[]) => unknown;
+// type iterableObject = { [key: string]: any };
 
 // //Make it work with objects as well and DRY
-// export const map = (fn, iter) => {
-// 	if (iter === undefined) {
-// 		return iter => {
-// 			if (typeof iter !== 'object') return;
+// export const map = <T, U extends [] | iterableObject>(
+// 	fn: MapFunction<T>,
+// 	iter?: U
+// ) => {
+// 	// if (iter === undefined) {
+// 	// 	return (iter: U) => {
+// 	// 		if (typeof iter !== 'object') return;
 
-// 			if (Array.isArray(iter)) {
-// 				return iter.map(fn);
-// 			} else {
-// 				const obj = {};
-// 				for (const k in iter) {
-// 					obj[k] = fn(iter[k]);
-// 				}
-// 				return obj;
-// 			}
-// 		};
-// 	}
+// 	// 		if (Array.isArray(iter)) {
+// 	// 			return iter.map(fn);
+// 	// 		} else {
+// 	// 			const obj = {};
+// 	// 			for (const k in iter) {
+// 	// 				obj[k] = fn(iter[k]);
+// 	// 			}
+// 	// 			return obj;
+// 	// 		}
+// 	// 	};
+// 	// }
 
 // 	if (typeof iter !== 'object') return;
 
 // 	if (Array.isArray(iter)) {
 // 		return iter.map(fn);
 // 	} else {
-// 		const obj = {};
+// 		const obj: iterableObject = {};
 // 		for (const k in iter) {
-// 			obj[k] = fn(iter[k]);
+// 			obj[k] = fn(iter[k] as any);
 // 		}
 // 		return obj;
 // 	}
 // };
+
+// const arr = [1, 2, 3];
+// const add: MapFunction<number> = (x: number) => x + 2;
+// const obj = { a: 1, b: 2, c: 3 };
+
+// const test1 = map(add, arr);
+// const test2 = map(add, obj);
+
+// hmm(test1);
+// hmm(test2);
 
 // export const pipe = (...fns) => x => {
 // 	const [head, ...tail] = fns;
@@ -97,25 +118,25 @@ hmm('hai');
 // 	return front.length > 0 ? compose(...front)(res) : res;
 // };
 
-// export const randomInt = (top, start = 0) => {
-// 	const range = top - start + 1;
-// 	return Math.floor(Math.random() * range + start);
-// };
+export const randomInt = (top: number, start = 0) => {
+	const range = top - start + 1;
+	return Math.floor(Math.random() * range + start);
+};
 
-// export const logger = x => {
-// 	hmm(x);
-// 	return x;
-// };
+export const logger = <T>(x: T) => {
+	hmm(x);
+	return x;
+};
 
-// export const head = arr => arr.slice(0, 1);
+export const head = <T>(arr: T[]) => arr.slice(0, 1)[0];
 
-// export const first = head;
+export const first = head;
 
-// export const last = arr => arr.slice(arr.length - 1);
+export const last = <T>(arr: T[]) => arr.slice(arr.length - 1)[0];
 
-// export const tail = arr => arr.slice(1, arr.length);
+export const tail = <T>(arr: T[]) => arr.slice(1, arr.length);
 
-// export const initial = arr => arr.slice(0, arr.length - 1);
+export const initial = <T>(arr: T[]) => arr.slice(0, arr.length - 1);
 
 // export const prop = (key, obj) => {
 // 	if (obj === undefined)
