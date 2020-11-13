9d224ab2d074e197a86ab1be29060563fb4516fa
Marlrus <36910211+Marlrus@users.noreply.github.com>
2020-11-13 00:10:11 -0500

    Finished impure utilities tests

--- __tests__/utilites.test.ts
+++ __tests__/utilites.test.ts
@@ -1,4 +1,4 @@
-import { hmm, err } from '../src/index';
+import { hmm, err, logger, startT, stopT, table } from '../src/index';
 
 test('hmm returns undefined', () => {
   expect(hmm()).toBe(undefined);
@@ -10,4 +10,20 @@ test('err with no args throws ¯_(ツ)_/¯', () => {
 
 test('err with message throws message', () => {
   expect(() => err('poop')).toThrow('poop');
+});
+
+test('logger returns value', () => {
+  expect(logger('hai')).toBe('hai');
+});
+
+test('startT returns undefined', () => {
+  expect(startT('start')).toBe(undefined);
+});
+
+test('stopT returns undefined', () => {
+  expect(stopT('end')).toBe(undefined);
+});
+
+test('table returns undefined', () => {
+  expect(table({})).toBe(undefined);
 });
